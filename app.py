ea8ac52344ad878750d24afe1351ee57ad50992c
Andrew Ho <70073200+RapidCancel@users.noreply.github.com>
2025-04-30 19:43:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,21 @@ from PySide6.QtWidgets import QApplication
 from login import LoginScreen
 from mainWindow import MainWindow
 
+from addCommission import AddCommissionWidget
+
 from reset import EZResetWidget     # Remove EZReset after
 
 def onLogin():
     mainWindow.show()
-    loginScreen.close()
+    loginScreen.deleteLater()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     loginScreen = LoginScreen(0)
     mainWindow = MainWindow()
+
+    #addCommission = AddCommissionWidget()
+    #addCommission.show()
 
     eZReset = EZResetWidget()
     eZReset.show()
